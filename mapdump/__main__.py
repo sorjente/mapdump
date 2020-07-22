8133917d76ebb481a235f51a5f8f08d3400b5179
Dimitris Kokkonis <kokkonisd@gmail.com>
2020-07-22 20:53:31 +0200

    Add boilerplate stuff for python package

---
+++ mapdump/__main__.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+##
+## @package mapdump
+##
+## The mapdump command-line tool, by
+##
+##                     _            __      
+##    _________  _____(_)__  ____  / /____ 
+##   / ___/ __ \/ ___/ / _ \/ __ \/ __/ _ \ 
+##  (__  ) /_/ / /  / /  __/ / / / /_/  __/
+## /____/\____/_/__/ /\___/_/ /_/\__/\___/ 
+##              /___/                      
+
+
+import sys
+import os
+import click
+
+from .definitions import VERSION
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+
