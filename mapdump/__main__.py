f535a451bd6bd629b5f56813ccbf4eebed652727
AyoubSabri <sabriayoub96@gmail.com>
2020-07-26 20:12:33 -0400

    Add basic front-end interface. More options to be added in the future.

--- mapdump/__main__.py
+++ mapdump/__main__.py
@@ -20,10 +20,21 @@ import click
 from .definitions import VERSION
 from .parser import get_raw_symbols, construct_memory_dict, get_memory_config
 
+@click.command()
+@click.argument('mapfile', nargs = 1)
+@click.version_option(version = VERSION,
+                      prog_name = "mapdump")
 
-def main():
-    print(get_memory_config("../../embedded/custom-pcb/hello_opencm3/build/main.map"))
-    print(construct_memory_dict(get_raw_symbols("../../embedded/custom-pcb/hello_opencm3/build/main.map")))
+def main(mapfile):
+    '''Analyzes a .map file in order to examine memory usage.'''
+    # check file extension
+    if mapfile.endswith('.map'):
+        print(get_memory_config(mapfile))
+        print(construct_memory_dict(get_raw_symbols(mapfile)))
+    else:
+        print("The file's extension is not correct. Please provide a .map file.")
+        exit(1)
 
 if __name__ == "__main__":
+    '''Program's entry point'''
     main()
