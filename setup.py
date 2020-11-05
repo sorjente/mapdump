d48198ffb13d54a452537b5d80600fb9ae6cc567
Dimitris Kokkonis <kokkonisd@gmail.com>
2020-11-05 16:08:41 +0100

    Fix license in setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
     packages = setuptools.find_packages(),
     classifiers = [
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
     install_requires = [ "click", "re" ],
