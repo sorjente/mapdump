d217e69b8396e0a220efc5246e6ebf79d6ef5197
Dimitris Kokkonis <kokkonisd@gmail.com>
2020-11-05 16:07:08 +0100

    Flesh out setup script

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/env/bin/python3
+
+import setuptools
+
+from mapdump.definitions import VERSION
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "mapdump",
+    version = VERSION,
+    author = "Ayoub Sabri, Dimitri Kokkonis",
+    author_email = "sabriayoub96@gmail.com, kokkonisd@gmail.com",
+    description = "A CLI tool that analyzes the memory usage of a binary via its .map file.",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/sorjente/mapdump",
+    packages = setuptools.find_packages(),
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires = [ "click", "re" ],
+    package_data = {'mapdump': []},
+    include_package_data = True,
+    entry_points = {'console_scripts': [
+        'mapdump = mapdump.__main__:main',
+    ], },
+)
