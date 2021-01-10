2195e8b15e9f0e9d5389daf1ffb3640f45243a32
AyoubSabri <sabriayoub96@gmail.com>
2021-01-10 15:29:42 -0500

    Run Black code formatter .py files

--- mapdump/definitions.py
+++ mapdump/definitions.py
@@ -7,11 +7,9 @@ VERSION = "1.0.1"
 # Memory section start address
 # Whitespace
 # Memory section size
-MEMORY_CONFIG_REGEX = r"([a-zA-Z0-9*_]+)"\
-                      r"[ \t]+"\
-                      r"(0x[a-zA-Z0-9]+)"\
-                      r"[ \t]+"\
-                      r"(0x[a-zA-Z0-9]+)"
+MEMORY_CONFIG_REGEX = (
+    r"([a-zA-Z0-9*_]+)" r"[ \t]+" r"(0x[a-zA-Z0-9]+)" r"[ \t]+" r"(0x[a-zA-Z0-9]+)"
+)
 
 # Section & symbol name
 # Whitespace
@@ -20,13 +18,15 @@ MEMORY_CONFIG_REGEX = r"([a-zA-Z0-9*_]+)"\
 # Symbol size
 # Whitespace
 # Path of .o file that contains the symbol
-LOCAL_SYMBOL_REGEX = r"\.([a-zA-Z0-9._]+)"\
-                     r"[\s]+"\
-                     r"(0x[a-zA-Z0-9]+)"\
-                     r"[\s]+"\
-                     r"(0x[a-zA-Z0-9]+)"\
-                     r"[\s]+"\
-                     r"(([a-zA-Z]|(\.\/)|(\/))[a-zA-Z0-9\/._-]+\.o)"
+LOCAL_SYMBOL_REGEX = (
+    r"\.([a-zA-Z0-9._]+)"
+    r"[\s]+"
+    r"(0x[a-zA-Z0-9]+)"
+    r"[\s]+"
+    r"(0x[a-zA-Z0-9]+)"
+    r"[\s]+"
+    r"(([a-zA-Z]|(\.\/)|(\/))[a-zA-Z0-9\/._-]+\.o)"
+)
 
 # Section name
 # Whitespace
@@ -39,17 +39,19 @@ LOCAL_SYMBOL_REGEX = r"\.([a-zA-Z0-9._]+)"\
 # Symbol start address
 # Whitespace
 # Symbol name
-LIB_SYMBOL_REGEX = r"\.([a-zA-Z0-9._]+)"\
-                   r"[\s]+"\
-                   r"(0x[a-zA-Z0-9]+)"\
-                   r"[\s]+"\
-                   r"(0x[a-zA-Z0-9]+)"\
-                   r"[\s]+"\
-                   r"(([a-zA-Z]|(\.\/)|(\/))[a-zA-Z0-9\/._-]+\.a\([a-zA-Z0-9_-]+\.o\))"\
-                   r"[\s]+"\
-                   r"0x[a-zA-Z0-9]+"\
-                   r"[\s]+"\
-                   r"([a-zA-Z0-9_]+)"
+LIB_SYMBOL_REGEX = (
+    r"\.([a-zA-Z0-9._]+)"
+    r"[\s]+"
+    r"(0x[a-zA-Z0-9]+)"
+    r"[\s]+"
+    r"(0x[a-zA-Z0-9]+)"
+    r"[\s]+"
+    r"(([a-zA-Z]|(\.\/)|(\/))[a-zA-Z0-9\/._-]+\.a\([a-zA-Z0-9_-]+\.o\))"
+    r"[\s]+"
+    r"0x[a-zA-Z0-9]+"
+    r"[\s]+"
+    r"([a-zA-Z0-9_]+)"
+)
 
 
 # COMMON section
@@ -62,17 +64,19 @@ LIB_SYMBOL_REGEX = r"\.([a-zA-Z0-9._]+)"\
 # Whitespace
 # Address (same as before)
 # Symbol name
-COMMON_SYMBOL_REGEX = r"COMMON"\
-                      r"[\s]+"\
-                      r"(0x[a-zA-Z0-9]+)"\
-                      r"[\s]+"\
-                      r"(0x[a-zA-Z0-9]+)"\
-                      r"[\s]+"\
-                      r"(([a-zA-Z]|(\.\/)|(\/))[a-zA-Z0-9\/._-]+\.o)"\
-                      r"[\s]+"\
-                      r"[a-zA-Z0-9]+"\
-                      r"[\s]+"\
-                      r"([a-zA-Z0-9_]+)"
+COMMON_SYMBOL_REGEX = (
+    r"COMMON"
+    r"[\s]+"
+    r"(0x[a-zA-Z0-9]+)"
+    r"[\s]+"
+    r"(0x[a-zA-Z0-9]+)"
+    r"[\s]+"
+    r"(([a-zA-Z]|(\.\/)|(\/))[a-zA-Z0-9\/._-]+\.o)"
+    r"[\s]+"
+    r"[a-zA-Z0-9]+"
+    r"[\s]+"
+    r"([a-zA-Z0-9_]+)"
+)
 
 
 # FILL entry
@@ -80,8 +84,6 @@ COMMON_SYMBOL_REGEX = r"COMMON"\
 # Start address
 # Whitespace
 # Size
-FILL_SYMBOL_REGEX = r"\*fill\*"\
-                    r"[\s]+"\
-                    r"(0x[a-zA-Z0-9]+)"\
-                    r"[\s]+"\
-                    r"(0x[a-zA-Z0-9]+)"
+FILL_SYMBOL_REGEX = (
+    r"\*fill\*" r"[\s]+" r"(0x[a-zA-Z0-9]+)" r"[\s]+" r"(0x[a-zA-Z0-9]+)"
+)
--- mapdump/symbol.py
+++ mapdump/symbol.py
@@ -4,9 +4,10 @@ from .memory import Memory
 
 
 class Symbol(object):
-    '''
+    """
     Describes symbols found in object files.
-    '''
+    """
+
     def __init__(self, name, address, size, section, file, lib=None):
         self.name = name
         self.address = address
@@ -15,22 +16,19 @@ class Symbol(object):
         self.file = file
         self.lib = lib
 
-
     def __str__(self):
         return f"{self.name} @ {self.address:#012x} ({self.size} bytes) [{self.file}]"
 
-
     def is_in_memory(self, memory):
-        return self.address >= memory.address and (self.address + self.size) <= (memory.address + memory.size)
-
+        return self.address >= memory.address and (self.address + self.size) <= (
+            memory.address + memory.size
+        )
 
     def is_external(self):
         return self.lib != None
 
-
     def is_common(self):
-        return self.section == 'COMMON'
-
+        return self.section == "COMMON"
 
     def is_fill(self):
-        return self.name == '*fill*'
+        return self.name == "*fill*"
